1d14b0cdf37ac57f9c0a7e9c8b823ed92ff385f8
Chris Hewell Garrett <chris@bitski.com>
2023-06-23 12:03:10 -0400

    wip

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -6,13 +6,37 @@
 
   import type { PageData } from './$types';
   import { onMount } from 'svelte';
+  import { createBitskiProvider } from 'bitski-provider';
+  import { BITSKI_APP_ID } from '$lib/constants';
+  import { EthMethod } from 'eth-provider-types';
+  import { Interface } from '@ethersproject/abi';
 
   export let data: PageData;
 
   let balances: any;
+  let provider: any;
 
   onMount(() => {
+    provider = createBitskiProvider({
+      appId: BITSKI_APP_ID,
+      signerBaseUrl: 'https://sign-next.bitski.com',
+      apiBaseUrl: 'http://127.0.0.1:5173/api',
+      transactionCallbackUrl: window.location.origin + '/app',
+      waas: {
+        userId: data.userId,
+      },
+    });
+
     pollForNfts();
+
+    if (data.result) {
+      const { result } = JSON.parse(atob(data.result));
+
+      provider.request({
+        method: EthMethod.eth_sendRawTransaction,
+        params: [result],
+      });
+    }
   });
 
   const claimNFT = async () => {
@@ -28,9 +52,86 @@
     }
   };
 
+  const iface1155 = new Interface([
+    {
+      name: 'safeTransferFrom',
+      inputs: [
+        {
+          name: 'from',
+          type: 'address',
+        },
+        {
+          name: 'to',
+          type: 'address',
+        },
+        {
+          name: 'id',
+          type: 'uint256',
+        },
+        {
+          name: 'amount',
+          type: 'uint256',
+        },
+        {
+          name: 'data',
+          type: 'bytes',
+        },
+      ],
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ]);
+
+  function create1155TransactionData(
+    address: string,
+    recipient: string,
+    quantity: number,
+    tokenId: string,
+  ) {
+    return iface1155.encodeFunctionData('safeTransferFrom', [
+      address,
+      recipient,
+      tokenId,
+      quantity,
+      '0x',
+    ]);
+  }
+
+  export const burnToken = async (contractAddress: string, tokenId: string): Promise<void> => {
+    const provider = createBitskiProvider({
+      appId: BITSKI_APP_ID,
+      signerBaseUrl: 'https://sign-next.bitski.com',
+      apiBaseUrl: 'http://127.0.0.1:5173/api',
+      transactionCallbackUrl: window.location.origin + '/app',
+      waas: {
+        userId: data.userId,
+      },
+    });
+
+    const erc1155TxnData = create1155TransactionData(
+      data.account,
+      '0x0000000000000000000000000000000000000000',
+      1,
+      tokenId,
+    );
+
+    const transaction = {
+      from: data.account,
+      to: contractAddress,
+      data: erc1155TxnData,
+      type: 2,
+    };
+
+    provider.request({
+      method: EthMethod.eth_sendTransaction,
+      params: [transaction],
+    });
+  };
+
   const pollForNfts = async () => {
     const response = await fetch(
-      `https://api.bitski.com/v2/balances?address=${data.account}&chainIds=80001&nfts=true`,
+      `https://api.bitski.com/v2/balances?address=${data.account}&chainIds=137&nfts=true`,
     );
     ({ balances } = await response.json());
 
@@ -60,7 +161,21 @@
     Claim NFT
   </button>
 
-  {balances}
+  <ul>
+    {#each balances || [] as token}
+      <li class="pb-4">
+        {token.id}
+        {token.contractAddress}
+        <!-- {JSON.stringify(token)} -->
+        <button
+          class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
+          on:click={() => burnToken(token.contractAddress, token.id)}
+        >
+          Burn Token
+        </button>
+      </li>
+    {/each}
+  </ul>
 
   <Explainer
     text="Users can now easily interact with your product's experiences e.g. viewing NFTs, tokens, and activity. If users want to self custody their wallet to use on external products, they'll need to claim their wallet on Bitski (view claim flow)"
